2a47328dfcee0c1f899cbaf03b301cbe004b71d1
DBaer75 <david.jonathan.baer@gmail.com>
2020-09-23 05:00:41 +0000

    complete

---
+++ my_custom_srv_msg_pkg/src/custom_service_server.cpp
@@ -0,0 +1,31 @@
+#include "ros/ros.h"
+#include "my_custom_srv_msg_pkg/MyCustomServiceMessage.h"
+
+bool my_callback(my_custom_srv_msg_pkg::MyCustomServiceMessage::Request  &req,
+                 my_custom_srv_msg_pkg::MyCustomServiceMessage::Response &res)
+{  
+  ROS_INFO("Request Data==> radius=%f, repetitions=%d", req.radius, req.repetitions); 
+  if (req.radius > 5.0)
+  {
+    res.success = true;
+    ROS_INFO("sending back response:true");
+  }
+  else
+  {
+    res.success = false;
+    ROS_INFO("sending back response:false");
+  }
+  
+  return true;
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "service_server");
+  ros::NodeHandle nh;
+
+  ros::ServiceServer my_service = nh.advertiseService("/my_service", my_callback); 
+  ros::spin();
+
+  return 0;
+}
